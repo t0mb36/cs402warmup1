5f88a0093b083957821c6567ffc5fe2c157cfdfa
t0mb36 <tombutler368@gmail.com>
2026-01-24 20:18:23 -0800

    Warmup1 File

--- warmup1.c
+++ warmup1.c
@@ -12,20 +12,67 @@
 #include <ctype.h>
 #include <time.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #include "my402list.h"
 #include "cs402.h"
 #include "my402list.h"
 
-#define MAX_LINE 2048
-#define TEN_MILLION_CENTS (10000000LL * 100LL)
+#define MAX_LINE_LENGTH 1024
+#define MAX_DESC_LENGTH 24
+#define MAX_AMOUNT 10000000
 
 typedef struct tagTxn {
     char type;              
     time_t ts;              
-    long long cents; 
-    char desc[1024];        
+    int cents; 
+    char desc[MAX_DESC_LENGTH + 1];        
 } Txn;
 
 
 
+static void PrintTransactions(My402list *list);
+
+
+
+
+
+
+
+
+
+
+static void PrintTransactions(My402list *list){
+	My402ListElem *elem;
+	long balance = 0;
+
+	char *border = "+-----------------+--------------------------+----------------+----------------+";
+	char *header = "|       Date      | Description              |         Amount |        Balance |";
+
+	printf("%s\n", border);
+    printf("%s\n", header);
+    printf("%s\n", border);
+
+    for (elem = My402ListFirst(list); elem != NULL; elem = My402ListNext(list, elem)){
+    	Txn *trans = (Txn *)(elem->object);
+
+    	if (trans->type == '+'){
+    		balance += trans->cents;
+    	} else {
+    		balance -= trans->cents;
+    	}
+    }
+
+    char *time_str = ctime(&trans->timestamp);
+    char date_buf[16]
+
+    strncpy(date_buf, time_str, 4);
+    strncpy(date_buf + 4, time_str + 4, 4);
+    strncpy(date_buf + 8, time_str + 8, 3);
+    strncpy(date_buf + 11, time_str + 20, 4);
+    date_buf[15] = '\0';
+
+    char amount_buf[16];
+    FormatAmount
+
+}
