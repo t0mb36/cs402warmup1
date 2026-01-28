1245f87ca8e72d18b3f3e88944b2bae698a02fd7
t0mb36 <tombutler368@gmail.com>
2026-01-27 21:37:20 -0800

    Adding comments, tests, readme

--- w1-README.txt
+++ w1-README.txt
@@ -9,12 +9,12 @@ Replace "(Comments?)" below with the command the grader should use to compile
 your program (it should simply be "make" or "make warmup1"; minor variation is
 also fine).
 
-    To compile your code, the grader should type: (Comments?)
+    To compile your code, the grader should type: make warmup1
 
 If you have additional instruction for the grader, replace "(Comments?)" with your
 instruction (or with the word "none" if you don't have additional instructions):
 
-    Additional instructions for building/running this assignment: (Comments?)
+    Additional instructions for building/running this assignment: none
 
 +-------------------------+
 | SELF-GRADING (Required) |
@@ -22,23 +22,23 @@ instruction (or with the word "none" if you don't have additional instructions):
 
 Replace each "?" below with a numeric value:
 
-(A) Doubly-linked Circular List : ? out of 40 pts
+(A) Doubly-linked Circular List : 40 out of 40 pts
 
-(B.1) Sort (file) : ? out of 30 pts
-(B.2) Sort (stdin) : ? out of 30 pts
+(B.1) Sort (file) : 30 out of 30 pts
+(B.2) Sort (stdin) : 30 out of 30 pts
 
-Missing required section(s) in README file : -? pts
-Submitted binary file : -? pts
-Cannot compile : -? pts
-Compiler warnings : -? pts
-"make clean" : -? pts
-Segmentation faults : -? pts
-Program never terminates : -? pts
-Separate compilation : -? pts
-Malformed input : -? pts
-Too slow : -? pts
-Bad commandline : -? pts
-Did not use My402List and My402ListElem to implement "sort" in (B) : -? pts
+Missing required section(s) in README file : -0 pts
+Submitted binary file : -0 pts
+Cannot compile : -0 pts
+Compiler warnings : -0 pts
+"make clean" : -0 pts
+Segmentation faults : -0 pts
+Program never terminates : -0 pts
+Separate compilation : -0 pts
+Malformed input : -0 pts
+Too slow : -0 pts
+Bad commandline : -0 pts
+Did not use My402List and My402ListElem to implement "sort" in (B) : -0 pts
 
 +---------------------------------+
 | BUGS / TESTS TO SKIP (Required) |
@@ -52,7 +52,7 @@ will not get plus points for them; if the garder won't run such tests in the
 minus points section, you will lose all the points there.)  If there's nothing
 the grader should skip, please replace "(Comments?)" with "none".
 
-Please skip the following tests: (Comments?)
+Please skip the following tests: none
 
 +--------------------------------------------------------------------------------------------+
 | ADDITIONAL INFORMATION FOR GRADER (Optional, but the grader should read what you add here) |
@@ -62,4 +62,8 @@ Please skip the following tests: (Comments?)
 | OTHER (Optional) - Not considered for grading |
 +-----------------------------------------------+
 
-Comments on design decisions: (Comments?)
+Comments on design decisions:
+
+For implementation of any numeric dollar amounts I went with an inplementation of cents rather than proceeding with floating point dollar amount, we learned this pattern in cs455x and wanted to implement this way as to avoid any unwanted and hard to track floating point arithmentic bugs.
+
+Straigh forward implementation. handled errors with verbosity inside of functions, also maintained list as sorted on every insert to avoid having to sort after insert (way too complex and wouldnt scale). Since we always start with an empty list, the list is always sorted and inserting into a sorted list is much easier.
--- warmup1.c
+++ warmup1.c
@@ -25,7 +25,8 @@ typedef struct tagTxn {
     char type;              
     time_t ts;              
     int cents; 
-    char desc[MAX_DESC_LENGTH + 1];        
+    char desc[MAX_DESC_LENGTH + 1];
+    int line_num;        
 } Txn;
 
 
@@ -42,29 +43,35 @@ static void FormatAmount(int cents, int is_negative, char *buf);
 int main(int argc, char *argv[]){
 	FILE *fp = NULL;
 
+	//must have 2 or 3 args
 	if (argc < 2 || argc > 3){
+		fprintf(stderr, "error: malformed command\n");
 		Usage();
 	}
 
+	//first arg must be "sort"
 	if (strcmp(argv[1], "sort") != 0){
-		fprintf(stderr, "error: unknown command '%s'\n", argv[1]);
+		fprintf(stderr, "error: malformed command, '%s' is not a valid commandline option\n", argv[1]);
 		Usage();
 	}
 
 	if (argc == 3){
 		struct stat st;
 		
+		//check if file exists
 		if (stat(argv[2], &st) != 0){
 			fprintf(stderr, "error: cannot open file '%s' - %s\n", 
                     argv[2], strerror(errno));
             exit(1);
 		}
 		
+		//check if input is directory
 		if (S_ISDIR(st.st_mode)){
 			fprintf(stderr, "error: '%s' is a directory\n", argv[2]);
             exit(1);
 		}
 
+		//Open file for reading
 		fp = fopen(argv[2], "r");
 		
 		if (fp == NULL){
@@ -76,6 +83,7 @@ int main(int argc, char *argv[]){
 		ProcessFile(fp, argv[2]);
 		fclose(fp);
 	} else {
+		//No file, read from stdin
 		ProcessFile(stdin, "stdin");
 	}
 	return 0;
@@ -88,7 +96,7 @@ static void Usage(void){
 
 static void ProcessFile(FILE *fp, const char *filename){
 	My402List list;
-	char line[MAX_LINE_LENGTH + 2];
+	char line[MAX_LINE_LENGTH + 2]; //+2 to detect lines > 1024
 	int line_num = 0;
 	int txn_count = 0;
 
@@ -97,10 +105,12 @@ static void ProcessFile(FILE *fp, const char *filename){
         exit(1);
 	}
 
+	//read file line by line
 	while (fgets(line, sizeof(line), fp) != NULL){
 		line_num++;
 		size_t len = strlen(line);
 
+		//Check for lines that are too long
 		if (len > 0 && line[len - 1] != '\n'){
 			if (len >= MAX_LINE_LENGTH){
 				fprintf(stderr, "error: line %d in '%s' is too long\n", 
@@ -109,14 +119,16 @@ static void ProcessFile(FILE *fp, const char *filename){
                 exit(1);
 			}
 		} else if (len > 0) {
+			//remove training /n
 			line[len - 1] = '\0';
 			len--;
 		}
-
+		//skip empty lines
 		if (len == 0){
 			continue;
 		}
 
+		//allocate new Txn object on heap, needs to persist
 		Txn *trans = (Txn *)malloc(sizeof(Txn));
 		if (trans == NULL){
 			fprintf(stderr, "error: malloc failed\n");
@@ -124,6 +136,7 @@ static void ProcessFile(FILE *fp, const char *filename){
             exit(1);
 		}
 
+		//parse line into struct
 		if (!ParseLine(line, trans, line_num, filename)){
 			free(trans);
 			FreeTxns(&list);
@@ -134,6 +147,7 @@ static void ProcessFile(FILE *fp, const char *filename){
 		txn_count++;
 	}
 
+	//Must have at least one transaction
 	if (txn_count == 0){
 		fprintf(stderr, "error: '%s' contains no valid transactions\n", filename);
         My402ListUnlinkAll(&list);
@@ -152,6 +166,7 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
 	char *ptr = line;
 	char *start = line;
 
+	//Split line by tab character
 	while (*ptr != '\0' && field_count < 4){
 		if (*ptr == '\t'){
 			*ptr = '\0';
@@ -162,14 +177,16 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
 		ptr++;
 	}
 
+	//capture last field 
 	if (field_count < 4){
 		fields[field_count] = start;
 		field_count++;
 	}
 
+	//Must have exactly 4 fields
 	if (field_count != 4) {
-        fprintf(stderr, "error: line %d in '%s' has incorrect number of fields\n", 
-                line_num, filename);
+        fprintf(stderr, "error: line %d in '%s' is not in the right format (incorrect number of fields)\n", 
+            line_num, filename);
         return FALSE;
     }
 
@@ -185,18 +202,21 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
     char *ts_str = fields[1];
     size_t ts_len = strlen(ts_str);
 
+    //must be 1 - 10 digits
     if (ts_len == 0 || ts_len >= 11){
     	fprintf(stderr, "error: line %d in '%s' has invalid timestamp\n", 
                 line_num, filename);
         return FALSE;
     }
 
+    //no leading zeros
     if (ts_str[0] == '0' && ts_len > 1){
         fprintf(stderr, "error: line %d in '%s' has timestamp with leading zero\n", 
                 line_num, filename);
         return FALSE;
     }
 
+    //all characters must be digits
     for (size_t i = 0; i < ts_len; i++){
         if (!isdigit((unsigned char)ts_str[i])){
             fprintf(stderr, "error: line %d in '%s' has non-numeric timestamp\n", 
@@ -214,8 +234,8 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
         return FALSE;
     }
 
-    time_t current_time = time(NULL);
-    
+    //Timestamp must be in the past
+    time_t current_time = time(NULL); 
     if ((time_t)ts_val >= current_time){
         fprintf(stderr, "error: line %d in '%s' has timestamp in the future\n", 
                 line_num, filename);
@@ -234,6 +254,7 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
         return FALSE;
     }
 
+    //Must have decimal point
     char *dec = strchr(amount_str, '.');
     if (dec == NULL){
     	fprintf(stderr, "error: line %d in '%s' has amount without decimal point\n", 
@@ -241,6 +262,7 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
         return FALSE;
     }
 
+    //Exactly 2 digits after decimal 
     size_t dec_len = strlen(dec + 1);
     if (dec_len != 2 || !isdigit((unsigned char)dec[1]) || !isdigit((unsigned char)dec[2])){
     	fprintf(stderr, "error: line %d in '%s' has invalid decimal places in amount\n", 
@@ -248,6 +270,7 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
         return FALSE;
     }
 
+    //Integer part must be 1-7 digits
     size_t int_len = dec - amount_str;
     if (int_len == 0 || int_len > 7){
     	fprintf(stderr, "error: line %d in '%s' has invalid integer part in amount\n", 
@@ -255,12 +278,14 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
         return FALSE;
     }
 
+    //No leading zeroes except 0.XX
     if (int_len > 1 && amount_str[0] == '0'){
     	fprintf(stderr, "error: line %d in '%s' has amount with leading zero\n", 
                 line_num, filename);
         return FALSE;
     }
 
+    //All chars before decimal must be digits
     for (size_t i = 0; i < int_len; i++){
     	if (!isdigit((unsigned char)amount_str[i])){
     		fprintf(stderr, "error: line %d in '%s' has non-numeric amount\n", 
@@ -269,6 +294,7 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
     	}
     }
 
+    //Convert to cents: split at decimal, combine dollars*100 + cents, ascii to int
     *dec = '\0';
     int dollars = atoi(amount_str);
     int cents = atoi(dec + 1);
@@ -287,19 +313,27 @@ static int ParseLine(char *line, Txn *trans, int line_num, const char *filename)
     	desc++;
     }
 
+    //check if empty after trimming (not allowed)
     if (*desc == '\0'){
     	fprintf(stderr, "error: line %d in '%s' has empty description\n", 
                 line_num, filename);
         return FALSE;
     }
 
+    //Copy description & truncate
     strncpy(trans->desc, desc, MAX_DESC_LENGTH);
     trans->desc[MAX_DESC_LENGTH] = '\0';
+    
+    trans->line_num = line_num;
 
     return TRUE;
 }
 
-
+/*
+ *Insert transaction into list, maintaining timestamp order so list is always sorted
+ *Exits on duplicate
+ *
+ */
 static void SortedInsert(My402List *list, Txn *trans, const char *filename){
 	My402ListElem *elem;
 
@@ -307,7 +341,7 @@ static void SortedInsert(My402List *list, Txn *trans, const char *filename){
 		Txn *curr = (Txn *)(elem->obj);
 
 		if (curr->ts == trans->ts){
-			fprintf(stderr, "error: duplicate timestamp in '%s'\n", filename);
+			fprintf(stderr, "error: line %d in '%s' has duplicate timestamp\n", trans->line_num, filename);
 			free(trans);
 			FreeTxns(list);
 			exit(1);
@@ -324,6 +358,7 @@ static void SortedInsert(My402List *list, Txn *trans, const char *filename){
 		}
 	}
 
+	//Append largest timestamp at end
 	if (!My402ListAppend(list, trans)){
 		fprintf(stderr, "error: failed to insert transaction\n");
         free(trans);
@@ -336,6 +371,7 @@ static void FormatAmount(int cents, int is_negative, char *buf){
 	int dollars = cents / 100;
 	int remainder = cents % 100;
 
+	//Overflow balance display
 	if (cents >= MAX_AMOUNT * 100){
 		if (is_negative){
 			strcpy(buf, "(?,???,???" ".??" ")");
@@ -345,9 +381,11 @@ static void FormatAmount(int cents, int is_negative, char *buf){
 		return;
 	}
 
+	//Format dollars.cents 
 	char num_buf[20];
 	sprintf(num_buf, "%d.%02d", dollars, remainder);
 
+	//Format output to have a comma
 	char format_buf[20];
 	int dec_pos = (int)(strchr(num_buf, '.') - num_buf);
 	int j = 0;
@@ -355,6 +393,8 @@ static void FormatAmount(int cents, int is_negative, char *buf){
 	for (int i = 0; i < dec_pos; i++){
 		format_buf[j] = num_buf[i];
 		j++;
+
+		//Insert comma every 3 digits
 		int remaining = dec_pos - i - 1;
 		if (remaining > 0 && remaining % 3 == 0){
 			format_buf[j] = ',';
@@ -362,8 +402,10 @@ static void FormatAmount(int cents, int is_negative, char *buf){
 		}
 	}
 
+	//Copy decimal part
 	strcpy(format_buf + j, num_buf + dec_pos);
 
+	//Format with width and paranthesis for neg/pos
 	if (is_negative){
 		sprintf(buf, "(%12s)", format_buf);
 	} else {
@@ -374,6 +416,9 @@ static void FormatAmount(int cents, int is_negative, char *buf){
 /*
  *Format date thats passed in as a cstring
  *PRE: the buf is of size buf[16] to fit with output format
+ *ctime() formate is "Wed Jan 19 21:25:02 1998"
+ *Function extracts day + mon (0-7), date (8-10), year (20-23) #'s are cstring array index'
+ * add null terminator at the end
  *
  */
 static void FormatDate(time_t ts, char *buf){
@@ -385,7 +430,11 @@ static void FormatDate(time_t ts, char *buf){
 
 	buf[15] = '\0';
 }
-
+/*
+ * Print all Txns as formatted table
+ * Each line follows spec formatting
+ *
+ */
 static void PrintTransactions(My402List *list){
 	My402ListElem *elem;
 	long balance = 0;
@@ -397,22 +446,27 @@ static void PrintTransactions(My402List *list){
     printf("%s\n", header);
     printf("%s\n", border);
 
+    //Print each transaction row
     for (elem = My402ListFirst(list); elem != NULL; elem = My402ListNext(list, elem)){
     	Txn *trans = (Txn *)(elem->obj);
 
+    	//Update running balance
     	if (trans->type == '+'){
     		balance += trans->cents;
     	} else {
     		balance -= trans->cents;
     	}
 
+    	//Format Date
     	char date_buf[16];
     	FormatDate(trans->ts, date_buf);
 
+    	//Format amount with 1/0 to track if withrawal or deposit
     	char amount_buf[16];
     	int is_negative = (trans->type == '-');
     	FormatAmount(trans->cents, is_negative, amount_buf);
 
+    	//Format balance with 1/0 to track negative or positive
     	char balance_buf[16];
     	long abs_balance;
     	int is_negative_balance;
